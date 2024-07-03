a02e45309e9ea7d814e8c497a30539f0f2bb79f4
AForce13 <andreist2004@gmail.com>
2024-07-03 21:00:54 +0300

    pistol and shotgun

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -9,11 +9,15 @@ const crouching_speed = 3.0
 ## Gravity and jump
 const jump_velocity = 7
 const grav_multiplier = 2
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") # Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 ## Mouselook
 const mouse_sens = 0.3
 ## Imports
 @onready var head = $Head
+@onready var animation_player = $Head/Camera3D/Weapons_Manager/FPS_Rig/AnimationPlayer
+## Misc
+var cur_wp = "empty"
 
 
 func _ready():
@@ -23,7 +27,8 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-deg_to_rad(event.relative.x * mouse_sens)) # Look left and right, yeah its rotate_y...
 		head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens)) # Look up and down
-		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89)) # Limit how much we can look up and down
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89)) 
+		# Limit how much we can look up and down
 
 func _physics_process(delta):
 	# Sprinting
@@ -50,5 +55,30 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
+	
+	# Draw weapon
+	if Input.is_action_just_pressed("mv_wp_1"):
+		if cur_wp == "pistol":
+			animation_player.play("pistol_hide")
+			cur_wp = "empty"
+		else:
+			animation_player.play("pistol_pull_out")
+			cur_wp = "pistol"
+	
+	if Input.is_action_just_pressed("mv_wp_2"):
+		if cur_wp == "pumpsg":
+			animation_player.play("pumpsg_hide")
+			cur_wp = "empty"
+		else:
+			animation_player.play("pumpsg_pull_out")
+			cur_wp = "pumpsg"
+	
+	# Shooting
+	if Input.is_action_pressed("mv_shoot"):
+		if !animation_player.is_playing():
+			if cur_wp == "pistol":
+				animation_player.play("pistol_shoot")
+			if cur_wp == "pumpsg":
+				animation_player.play("pumpsg_shoot")
 
 	move_and_slide()
